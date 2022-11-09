041626b17cef7ab2973f6ecac712e704c5fb1b14
HilarioX <juninho.gomes1234@gmail.com>
2022-11-09 16:28:45 -0300

    feat: add button for briefing response

--- src/views/start/index.js
+++ src/views/start/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Input, Button, Label } from 'reactstrap';
 import { api } from '../../services/api';
 import { toast } from 'react-toastify';
@@ -6,6 +6,8 @@ import { ErrorToast, SuccessToast } from '../components/toasts/Error';
 
 const start = () => {
   const [register, setRegister] = useState('');
+  const [grades, setGrades] = useState([]);
+  const [situation, setSituation] = useState(false);
 
   const postRegister = async () => {
     try {
@@ -27,47 +29,92 @@ const start = () => {
     }
   };
 
-  // const getResults = async () => {
-  //   try {
-  //     const student = await api.get('/activity/actual');
-  //     const results = await api.get(`/results/student/${student.registration}`);
+  const postBriefing = async (avaliation) => {
+    try {
+      const student = await api.get('/activity/actual');
 
-  //     results.array.forEach(result => {
-  //       if (result.stage === 7) {
-  //         return true;
-  //       }        
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //     toast.error(<ErrorToast description={error}/>, {
-  //       icon: false,
-  //       hideProgressBar: true,
-  //     });
-  //   }
-  // };
-
-  // if (getResults()) {
-  //   return (
-  //     <>
-  //       <div className='m-5'>
-  //       <h1>O briefing está de acordo?</h1>
-
-  //       </div>
-  //     </>
-  //   )
-  // }
-  return (
-    <>
-      <h1>Iniciar atividade</h1>
-      <p>Digite o registro do aluno que irá iniciar a atividade.</p>
-      <div className='mt-5'>
-        <Label>Registro do Aluno</Label>
-        <Input className="mb-2" type="text" onChange={(e) => setRegister(e.target.value)}/>
-        <Button color="primary" onClick={() => postRegister()}>Iniciar</Button>
-      </div>
+      await api.post(`/results/`,
+        { 
+          body: {
+            stage: 7,
+            registration: student.data.actual_student.registration,
+            grade: avaliation 
+          }
+        }
+      );
       
-    </>
-  );
+      toast.success(
+        <SuccessToast description="Avalição enviada!" />,
+        {
+          icon: false,
+          hideProgressBar: true,
+        }
+      );
+    } catch {
+      console.log(error);
+      toast.error(<ErrorToast description="Avaliação inválida!" />, {
+        icon: false,
+        hideProgressBar: true,
+      });
+    }
+  }
+
+  const getGrades = async () => {
+    try {
+      const student = await api.get('/activity/actual');
+      const results = await api.get(`/results/student/${student.data.actual_student.registration}`);
+      setGrades(results.data.results);
+
+      grades.forEach(grade => {
+        if (grade.stage === 6) {
+          setSituation(true);
+        }
+
+        if (grade.stage === 7) {
+          setSituation(false);
+        }
+      });      
+    } catch (error) {
+      console.log(error);
+      toast.error(<ErrorToast description={error}/>, {
+        icon: false,
+        hideProgressBar: true,
+      });
+    }
+  };
+
+  useEffect(() => {
+    getGrades();
+  }, [grades])
+
+  return (
+    situation === true ? (
+      <>
+        <h1>Iniciar atividade</h1>
+        <p>Digite o registro do aluno que irá iniciar a atividade.</p>
+        <div className='mt-5'>
+          <Label>Registro do Aluno</Label>
+          <Input className="mb-2" type="text" onChange={(e) => setRegister(e.target.value)}/>
+          <Button color="primary" onClick={() => postRegister()}>Iniciar</Button>
+        </div>
+        <div className='m-5 text-center'>
+        <h1>O briefing está de acordo?</h1>
+          <Button className="m-4 mt-3" color="primary" onClick={() => postBriefing(true)}>Sim</Button>
+          <Button className="m-4 mt-3" color="primary" onClick={() => postBriefing(false)}>Não</Button>
+        </div>
+      </>
+    ) : (
+        <>
+        <h1>Iniciar atividade</h1>
+        <p>Digite o registro do aluno que irá iniciar a atividade.</p>
+        <div className='mt-5'>
+            <Label>Registro do Aluno</Label>
+            <Input className="mb-2" type="text" onChange={(e) => setRegister(e.target.value)} />
+            <Button color="primary" onClick={() => postRegister()}>Iniciar</Button>
+          </div>  
+        </>
+      )
+    )
 };
 
 export default start;
