681e4235105c0b5072b5d7a4c54f64b3656df294
Hilario <juninho.gomes1234@gmail.com>
2023-01-30 10:34:07 -0300

    feat: search by name

--- src/views/reports/index.js
+++ src/views/reports/index.js
@@ -1,4 +1,4 @@
-import { Table } from 'reactstrap';
+import { Table, Label, Input } from 'reactstrap';
 import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 import { useParams } from 'react-router-dom';
@@ -6,10 +6,11 @@ import { useParams } from 'react-router-dom';
 const notasUsers = () => {
   const { stage } = useParams();
   const [notas, setNotas] = useState([]);
+  const [name, setName] = useState('');
 
   const handleResults = async () => {
     try {
-      const results = await api.get(`/results/stage/${stage}`);
+      const results = await api.get(`/results/stage/${stage}?name=${name}`);
       setNotas(results.data.results);
       console.log(notas);
     } catch (error) {
@@ -18,22 +19,30 @@ const notasUsers = () => {
   };
 
   useEffect(() => {
-    handleResults();
-  }, []);
-
-  useEffect(() => {
     console.log(notas);
+    handleResults();
   }, [notas]);
+
 
   return (
     <>
+      <div className="mt-5">
+        <Label>Nome do aluno</Label>
+        <Input
+          className="mb-2"
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
       <h1>Etapa {stage}</h1>
       {!notas ? null : (
         <Table responsive>
           <thead>
             <tr>
               <th>REGISTRO</th>
+              <th>NOME</th>
               <th>ACERTOS</th>
+              <th>DATA DE AVALIAÇÃO</th>
             </tr>
           </thead>
           <tbody>
@@ -43,7 +52,13 @@ const notasUsers = () => {
                   <span className="align-middle">{nota.registration}</span>
                 </td>
                 <td>
+                  <span className='align-middle'>{nota.name}</span>
+                </td>
+                <td>
                   <span className="align-middle">{nota.grade}</span>
+                </td>
+                <td>
+                  <span className='align-middle'>{nota.created_at}</span>
                 </td>
               </tr>
             ))}
