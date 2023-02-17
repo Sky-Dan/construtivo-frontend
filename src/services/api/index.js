3aa924f1a1578ec099f4b2313616a33fdc377b59
Hilario <juninho.gomes1234@gmail.com>
2023-02-17 16:07:30 -0300

    fix

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,7 +1,5 @@
-import { toast } from 'react-toastify';
 import axiosLibrary from 'axios';
 import { getUserData } from '../../utility/Utils';
-import { WarningToast } from '../../views/components/toasts/Error';
 
 
 const user = getUserData();
@@ -11,17 +9,5 @@ export const api = axiosLibrary.create({
 });
 
 if (user) {
-  api.get('/sessions/', {
-    headers: {
-      authorization : `Bearer ${user.token}`
-    }
-  }).then(() => {
     api.defaults.headers.common['authorization'] = `Bearer ${user.token}`;
-  }).catch(() => {
-    localStorage.removeItem('@ajinomotoSafeLife:userData');
-    toast.warning(<WarningToast description= 'Sua sessão foi encerrada. Por favor faça novamente login' />, {
-      icon: false,
-      hideProgressBar: true,
-    });
-  })
 }
